e3037081931b0c7cfb0b19ffdd9d36fdab1db199
Joel Midstjärna <joel.midstjarna@gmail.com>
2019-12-14 20:34:40 +0100

    Added appveyor script

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-python -m coverage run --source=./pycstruct -m unittest discover -s tests/
-python -m coverage report -m
+python3 -m coverage run --source=./pycstruct -m unittest discover -s tests/
+python3 -m coverage report -m
