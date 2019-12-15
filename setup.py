42ad2d3e01d03afcc8722bd16d6fb914f57b44d3
Joel Midstjärna <joel.midstjarna@gmail.com>
2019-12-15 19:19:11 +0100

    Using tox

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycstruct',
+      version='0.1',
+      description='Binary data handling in python',
+      url='http://github.com/midstar/pycstruct',
+      author='Joel Midstjärna',
+      author_email='joel.midstjarna@gmail.com',
+      license='MIT',
+      packages=['pycstruct'],
+      zip_safe=False)
