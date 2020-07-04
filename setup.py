0a609dc88efb84e4d126364a09d18f9c34e265aa
Joel Midstjärna <joel.midstjarna@gmail.com>
2020-07-04 21:53:30 +0200

    Fixed PyPi upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except:
       long_description = ""
 
 setup(name='pycstruct',
-      version='0.4.2',
+      version='0.4.3',
       description='Binary data handling in Python using dictionaries',
       long_description=long_description,
        long_description_content_type="text/markdown",
