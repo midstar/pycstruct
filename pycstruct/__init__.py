0a268935727d805be23f96c422d0ce8674acfeae
Joel Midstjärna <joel.midstjarna@gmail.com>
2021-02-21 19:40:42 +0100

    Fixed lint warnings in __init__py

--- pycstruct/__init__.py
+++ pycstruct/__init__.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+"""pycstruct library
+
+pycstruct is a python library for converting binary data to and from ordinary
+python dictionaries or specific instance objects.
+
+Data is defined similar to what is done in C language structs, unions,
+bitfields and enums.
+
+Typical usage of this library is read/write binary files or binary data
+transmitted over a network.
+
+For more information see:
+
+https://github.com/midstar/pycstruct
+"""
+
 from pycstruct.pycstruct import StructDef
 from pycstruct.pycstruct import BitfieldDef
 from pycstruct.pycstruct import EnumDef
