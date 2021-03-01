351f0c3b4cff44cb42f87cce083b79bf05a8dfd3
Joel Midstjärna <joel.midstjarna@gmail.com>
2021-03-01 16:35:52 +0100

    Bumped version to 0.9.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except:
       long_description = ""
 
 setup(name='pycstruct',
-      version='0.9.0',
+      version='0.9.1',
       description='Binary data handling in Python using dictionaries',
       long_description=long_description,
        long_description_content_type="text/markdown",
